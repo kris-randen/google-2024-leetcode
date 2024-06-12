2f7db39e4764604f436536f6620de3a985b0e211
kris-randen <kris.randen@gmail.com>
2024-06-13 02:28:34 +0530

    Solved 05 Longest Palindromic Substring. Runtime 7614 ms Beats 9.24% of users with Python3. Memory 112.02 MB Beats 6.60% of users with Python3

--- 01 Dynamic Programming/05_longest_palindromic_substring_medium.py
+++ 01 Dynamic Programming/05_longest_palindromic_substring_medium.py
@@ -31,3 +31,26 @@ Constraints:
 s consist of only digits and English letters.
 
 """
+
+def longestPalindromicSubstring(s):
+    if len(s) < 2: return s
+    n = len(s); dic = dict(); longest = (0,0)
+    for i in range(n - 1):
+        dic[(i,i)] = 1; dic[(i + 1, i)] = 1
+    dic[(n-1, n-1)] = 1
+    for d in range(2, n + 1):
+        for i in range(n - d + 1):
+            if dic[(i + 1, i + d - 2)] and s[i] == s[i + d - 1]:
+                dic[(i, i + d - 1)] = 1
+                longest = (i, i + d - 1)
+            else:
+                dic[(i, i + d - 1)] = 0
+    return s[longest[0]: longest[1] + 1]
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        return longestPalindromicSubstring(s)
+
+if __name__ == '__main__':
+    ps = longestPalindromicSubstring("babad")
+    print(ps)
